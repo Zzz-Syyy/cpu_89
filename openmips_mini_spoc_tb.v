f419a74cb25d6657da1b98a0ab80e27e3781e65f
Zzz-Syyy <59228409+Zzz-Syyy@users.noreply.github.com>
2021-04-24 16:40:59 +0800

    Add files via upload

--- openmips_mini_spoc_tb.v
+++ openmips_mini_spoc_tb.v
@@ -25,37 +25,42 @@ module openmips_min_sopc_tb();
 
   reg     CLOCK_50;
   reg     rst;
-//  reg CLOCK_inst;
-  wire [7:0] o_seg;
-  wire [7:0] o_sel;
+  reg CLOCK_inst;
+//  wire [7:0] o_seg;
+//  wire [7:0] o_sel;
   wire [31:0] reg28; 
        
   initial begin
-    CLOCK_50 = 1'b0;
-//    CLOCK_inst=1'b0;
-    forever #10 CLOCK_50 = ~CLOCK_50;
-//    forever #5 CLOCK_inst=~CLOCK_inst;
+    CLOCK_50 = 1'b1;
+
+    forever #2 CLOCK_50 = ~CLOCK_50;
+//    forever #1 CLOCK_inst=~CLOCK_inst;//    CLOCK_inst=1'b1;
   end
       
   initial begin
+    CLOCK_inst=1'b1;
+    forever #1 CLOCK_inst=~CLOCK_inst;
+  end
+  
+  initial begin
     rst = 1'b1;
     #1 rst= 1'b0;
-    #999999 $stop;
+    // #999999 $stop;
   end
        
-//  openmips_min_sopc openmips_min_sopc0(
-//		.clk(CLOCK_50),
-//		.rst(rst),
-//	    .reg28(reg28),
-//        .clk_inst(CLOCK_inst)
-//	);
+  openmips_min_sopc openmips_min_sopc0(
+		.clk(CLOCK_50),
+		.rst(rst),
+	    .result(reg28),
+        .clk_inst(CLOCK_inst)
+	);
 
-    CPU_top CPU_top(
-        .clk(CLOCK_50),
-        .rst(rst),
-        .o_seg(o_seg),
-        .o_sel(o_sel),
-        .reg28(reg28)
-    );
+//    CPU_top CPU_top(
+//        .clk(CLOCK_50),
+//        .rst(rst),
+//        .o_seg(o_seg),
+//        .o_sel(o_sel),
+//        .reg28(reg28)
+//    );
 
 endmodule
